aea4c164d6f584c27d425ca0e0d1c58e7c4694c2
1u1ucas <lucasleveque17@gmail.com>
2025-02-18 09:44:09 +0100

    --

--- src/components/ RoutesWrapper.tsx
+++ src/components/ RoutesWrapper.tsx
@@ -1,19 +1,57 @@
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import SigninPage from "../pages/auth/SigninPage";
 import SignupPage from "../pages/auth/SignupPage";
 import HomePage from "../pages/home/HomePage";
 import BookingPage from "../pages/booking/BookingPage";
 import BookingSinglePage from "../pages/booking/[id]";
+import Classroom from "../pages/admin/classroom";
+
 
 const RoutesWrapper = () => {
+const [isAuthenticated, setIsAuthenticated] = useState(false);
+const [isAdmin, setIsAdmin] = useState(false);
+
+  const checkToken = () => {
+    const token = localStorage.getItem("jwtToken");
+    if (token) {
+      try {
+        const tokenData = token.split('.')[1];
+        const decodedToken = atob(tokenData);
+        const parsedToken = JSON.parse(decodedToken);
+        if (parsedToken) {
+          setIsAuthenticated(true);
+          if (parsedToken.role === "admin") {
+            setIsAdmin(true);
+          }
+        }
+      } catch (error) {
+        console.error("Error parsing token:", error);
+      }
+    } else {
+      setIsAuthenticated(false);
+      setIsAdmin(false);
+    }
+  };
+
+   useEffect(() => {
+      checkToken();
+      window.addEventListener("storage", checkToken);
+      return () => {
+        window.removeEventListener("storage", checkToken);
+      };
+    }, []);
+
+
   return (
     <div className=" mt-12 px-6 lg:px-12">
       <Routes>
         <Route path="/login" element={<SigninPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/booking" element={<BookingPage />} />
         <Route path="/booking/:id" element={<BookingSinglePage />} />
+        <Route path="/classroom" element={<Classroom isAdmin={isAdmin}/>} />   
       </Routes>
     </div>
   );
--- src/components/ClassroomCard.tsx
+++ src/components/ClassroomCard.tsx
@@ -1,25 +1,63 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { ClassroomType } from '../types/classroom.type';
 
 interface ClassroomCardProps {
   classroom: ClassroomType;
+  isAdmin?: boolean;
 }
 
-const ClassroomCard: React.FC<ClassroomCardProps> = ({ classroom }) => {
+const ClassroomCard: React.FC<ClassroomCardProps> = ({ classroom, isAdmin }) => {
+  const [editing, setEditing] = useState(false);
+  const [classroomInfo, setClassroomInfo] = useState<ClassroomType>(classroom);
+
+
     const navigate = useNavigate();
+
 
 
      const onClick = () => {
     navigate(`/booking/${classroom.id}`);
      }
 
+     const onEdit = () => {
+        setEditing(true);
+        setClassroomInfo(classroom);
+      console.log('edit classroom : ', classroomInfo);
+      }
+
+     const onDelete = () => {
+        console.log('delete classroom');
+      };
+
   return (
     <div className="card bg-white shadow-md rounded-lg p-6">
+      {editing ? (
+        <>
       <h2 className="text-xl font-bold mb-2">{classroom.name}</h2>
       <p className="text-gray-700 mb-2">{classroom.description}</p>
       <p className="text-gray-700 mb-2">Capacité: {classroom.capacity}</p>
       <p className="text-gray-700 mb-2">disponible: {classroom.isAvailble ? 'Non' : 'Oui'}</p>
-      <button onClick={onClick}> Voir le calendrier </button>
+      <div>
+        <button onClick={onClick}> Voir le calendrier </button>
+        {isAdmin && (
+          <>
+          <button onClick={onEdit} className="bg-blue-500 text-white px-2 py-1 rounded-lg">Modifier</button>
+          <button onClick={onDelete} className="bg-red-500 text-white px-2 py-1 rounded-lg">Supprimer</button>
+          </>
+        )}
+      </div>
+      </>
+      ) : (
+        <>
+        <input type="text" value={classroomInfo.name} onChange={(e) => setClassroomInfo({ ...classroomInfo, name: e.target.value })} />
+        <input type="text" value={classroomInfo.description} onChange={(e) => setClassroomInfo({ ...classroomInfo, description: e.target.value })} />
+        <input type="number" value={classroomInfo.capacity} onChange={(e) => setClassroomInfo({ ...classroomInfo, capacity: parseInt(e.target.value) })} />
+        <input type="checkbox" checked={classroomInfo.isAvailble} onChange={(e) => setClassroomInfo({ ...classroomInfo, isAvailble: e.target.checked })} />
+        <button>Save</button>
+        </>
+      )}
+
     </div>
   );
 };
